99d35c0b75899c21b303f649f404fb88c277be91
RageRoringTiger <41753994+RageRoringTiger@users.noreply.github.com>
2020-06-28 07:07:39 -0500

    Update Basketball.java

--- src/main/java/me/rageroringtiger/basketball/Basketball.java
+++ src/main/java/me/rageroringtiger/basketball/Basketball.java
@@ -7,8 +7,14 @@ public final class Basketball extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Minecraft-Basketball has loaded.");
+        loadConfig();
         getServer().getPluginManager().registerEvents(new UseBasketBallEvent(), this);
+        System.out.println("Minecraft-Basketball has loaded.");
+    }
+
+    public void loadConfig(){
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
     }
 
 }
