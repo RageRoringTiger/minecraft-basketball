b84afaee44aff92ca55cc8c84ecd30a22ed16a91
RageRoringTiger <41753994+RageRoringTiger@users.noreply.github.com>
2020-06-28 04:21:31 -0500

    QuickSave

---
+++ src/main/java/me/rageroringtiger/basketball/Basketball.java
@@ -0,0 +1,13 @@
+package me.rageroringtiger.basketball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Basketball extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Minecraft-Basketball has loaded.");
+    }
+
+}
---
+++ src/main/java/me/rageroringtiger/basketball/UseBasketBallEvent.java
@@ -0,0 +1,23 @@
+package me.rageroringtiger.basketball;
+
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class UseBasketBallEvent implements Listener {
+    @EventHandler
+    public void onPlayerRightClick(PlayerInteractEvent event){
+        Player p = event.getPlayer();
+        Action a = event.getAction();
+        ArmorStand armorstand = (ArmorStand) p.getWorld().spawnEntity(p.getLocation(), EntityType.ARMOR_STAND);
+        ItemStack 
+        if (a == Action.RIGHT_CLICK_AIR){
+            armorstand.getEquipment().setHelmet();
+        }
+    }
+}
