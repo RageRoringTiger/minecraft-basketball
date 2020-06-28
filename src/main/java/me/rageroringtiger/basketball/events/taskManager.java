9c1b9fbfe2d50d44878312a6f6d1509c30513d9f
RageRoringTiger <41753994+RageRoringTiger@users.noreply.github.com>
2020-06-28 08:42:44 -0500

    Create taskManager.java

---
+++ src/main/java/me/rageroringtiger/basketball/events/taskManager.java
@@ -0,0 +1,10 @@
+package me.rageroringtiger.basketball.events;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class taskManager extends BukkitRunnable {
+    @Override
+    public void run() {
+        
+    }
+}
